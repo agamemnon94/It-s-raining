20b8d90db6853f67a4daac7e2a9b9dc6e80a0da5
Agamemnon94 <94798272+agamemnon94@users.noreply.github.com>
2022-06-27 22:40:25 +0200

    first comit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+let body = document.querySelector('body');
+
+
+
+setInterval(rainFall, 10);
+
+// let main = document.querySelector('main');
+
+function rainFall() {
+
+  let waterDrop = document.createElement('i');
+  // let waterDrop = document.createElement('img');
+
+  waterDrop.classList.add('fa-solid');
+  waterDrop.classList.add('fa-droplet');
+  // waterDrop.classList.add('drop');
+  // waterDrop.src = '/assets/img/Tennis_ball_3.svg.png';
+
+  waterDrop.style.fontSize = Math.random() * 7 + 'px';
+  // waterDrop.style.width = Math.random() * 10 + 'px';
+  // waterDrop.style.height = Math.random() * 50 + 'px';
+  waterDrop.style.animationDuration = Math.random() * 3.5 + 's';
+  waterDrop.style.opacity = Math.random() + 0.2;
+  waterDrop.style.left = Math.random() * window.innerWidth + 'px';
+
+  body.appendChild(waterDrop);
+
+  setTimeout(() => {
+    waterDrop.remove();
+  }, 4000)
+}
+console.log(body);
